a43c7e907a34f5e595c8a7ac8583ccc6b921ef18
Alexis Sellier <self@cloudhead.io>
2021-10-14 09:48:52 +0200

    Update rust-toolchain
    Signed-off-by: Alexis Sellier <self@cloudhead.io>
    Co-authored-by: Thomas Scholtes <geigerzaehler@axiom.fm>

--- org-node/src/client.rs
+++ org-node/src/client.rs
@@ -248,14 +248,14 @@ impl Client {
                     .map_err(|_| Error::Reply("GetPeers".to_string()))
             }
             Request::TrackProject(urn, timeout, reply) => {
-                let project = Client::get_project_head(&urn, &api).await?;
+                let project = Client::get_project_head(&urn, api).await?;
 
                 // Don't track projects that already exist locally.
                 if let Some(Head { remote, branch }) = project {
                     tracing::debug!(target: "org-node", "Project {} exists, (re-)setting head", urn);
 
                     // Set the project head if it isn't already set.
-                    Client::set_head(&urn, &remote, &branch, &paths)?;
+                    Client::set_head(&urn, &remote, &branch, paths)?;
 
                     return reply
                         .send(Ok(None))
@@ -268,13 +268,13 @@ impl Client {
                 while let Some(peer) = peers.next().await {
                     if let Ok(tracked) = Client::track_project(api, &urn, &peer).await {
                         let response = if tracked {
-                            let Head { remote, branch } = Client::get_project_head(&urn, &api)
+                            let Head { remote, branch } = Client::get_project_head(&urn, api)
                                 .await?
                                 .expect("a project that was just tracked should exist");
                             // Tracking doesn't automatically set the repository head, we have to do it
                             // manually. We set the head to the default branch of the project
                             // maintainer.
-                            Client::set_head(&urn, &remote, &branch, &paths)?;
+                            Client::set_head(&urn, &remote, &branch, paths)?;
 
                             Some(peer.peer_id)
                         } else {
@@ -370,7 +370,7 @@ impl Client {
 
         tracing::debug!(target: "org-node", "Setting ref {:?} -> {:?}", &local_branch_ref, oid);
         repository
-            .reference(&local_branch_ref, oid, true, "set-local-branch (org-node)")
+            .reference(local_branch_ref, oid, true, "set-local-branch (org-node)")
             .map_err(|e| Error::SetHead(Box::new(e)))?;
 
         tracing::debug!(target: "org-node", "Setting ref {:?} -> {:?}", &branch_ref, oid);
@@ -380,7 +380,7 @@ impl Client {
 
         tracing::debug!(target: "org-node", "Setting ref {:?} -> {:?}", &head, local_branch_ref);
         repository
-            .reference_symbolic(&head, &local_branch_ref, true, "set-head (org-node)")
+            .reference_symbolic(head, local_branch_ref, true, "set-head (org-node)")
             .map_err(|e| Error::SetHead(Box::new(e)))?;
 
         Ok(())
