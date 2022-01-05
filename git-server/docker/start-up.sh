923984f332405327815681b5efbd76cc754453ac
Alexis Sellier <self@cloudhead.io>
2022-01-05 11:40:09 +0100

    git-server: Update docker startup script
    Signed-off-by: Alexis Sellier <self@cloudhead.io>

--- git-server/docker/start-up.sh
+++ git-server/docker/start-up.sh
@@ -9,8 +9,9 @@ main () {
     # The destination is a Docker mounted volume so we postpone copying until the startup of the entire container, when the volume is already mounted.
     mkdir --parents /app/radicle/git/hooks
     cp --force /usr/local/bin/pre-receive /app/radicle/git/hooks/pre-receive
+    cp --force /usr/local/bin/post-receive /app/radicle/git/hooks/post-receive
 
-    exec /usr/local/bin/radicle-git-server --git-receive-pack --identity /app/radicle/identity --root /app/radicle/root --listen 0.0.0.0:8778
+    exec /usr/local/bin/radicle-git-server --git-receive-pack --root /app/radicle/root --listen 0.0.0.0:8778
 }
 
 main "$@"
