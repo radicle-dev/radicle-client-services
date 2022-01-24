746a2d31ffbf08614e9776f3133060e03997e2ff
Adam Szkoda <adaszko@gmail.com>
2022-01-24 15:40:39 +0100

    Initialize working directory with radicle-service-init
    Signed-off-by: Adam Szkoda <adaszko@gmail.com>

--- git-server/docker/start-up.sh
+++ git-server/docker/start-up.sh
@@ -7,6 +7,8 @@ set -o errtrace
 
 main () {
     # The destination is a Docker mounted volume so we postpone copying until the startup of the entire container, when the volume is already mounted.
+    radicle-service-init --root /app/radicle --identity /app/radicle/identity
+
     mkdir --parents /app/radicle/git/hooks
     cp --force /usr/local/bin/pre-receive /app/radicle/git/hooks/pre-receive
     cp --force /usr/local/bin/post-receive /app/radicle/git/hooks/post-receive
