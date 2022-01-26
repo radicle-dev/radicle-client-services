9b88fbd9dbde0b95961fe329a3513b2e35536703
Adam Szkoda <adaszko@gmail.com>
2022-01-26 09:32:23 +0100

    Fix deployment issues
    * Add --allow-unauthorized-keys
    * Fix services startup issues
    * Stop building the deprecated org-node in CI
    * Install latest git for SSH keys support
    * Fix git hooks location
    
    Signed-off-by: Alexis Sellier <self@cloudhead.io>

--- git-server/docker/start-up.sh
+++ git-server/docker/start-up.sh
@@ -6,14 +6,12 @@ set -o pipefail
 set -o errtrace
 
 main () {
-    # The destination is a Docker mounted volume so we postpone copying until the startup of the entire container, when the volume is already mounted.
-    radicle-service-init --root /app/radicle --identity /app/radicle/identity
+    # The destination is a Docker mounted volume so we postpone initialization until the startup of the entire container, when the volume is already mounted.
+    radicle-service-init --root /app/radicle/root --identity /app/radicle/identity
+    cp --force /usr/local/bin/pre-receive /app/radicle/root/git/hooks/pre-receive
+    cp --force /usr/local/bin/post-receive /app/radicle/root/git/hooks/post-receive
 
-    mkdir --parents /app/radicle/git/hooks
-    cp --force /usr/local/bin/pre-receive /app/radicle/git/hooks/pre-receive
-    cp --force /usr/local/bin/post-receive /app/radicle/git/hooks/post-receive
-
-    exec /usr/local/bin/radicle-git-server --git-receive-pack --root /app/radicle/root --listen 0.0.0.0:8778
+    exec /usr/local/bin/radicle-git-server --git-receive-pack --allow-unauthorized-keys --root /app/radicle/root --listen 0.0.0.0:8778
 }
 
 main "$@"
