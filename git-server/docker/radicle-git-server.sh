3aeb0ae5b337965464024bff77cdaf379a4d7178
Adam Szkoda <adaszko@gmail.com>
2022-06-28 10:06:31 +0200

    Pass RAD_HOME from docker-compose for flexibility
    Signed-off-by: Adam Szkoda <adaszko@gmail.com>

--- git-server/docker/radicle-git-server.sh
+++ git-server/docker/radicle-git-server.sh
@@ -6,9 +6,13 @@ set -o pipefail
 set -o errtrace
 
 main () {
+    if [[ -z ${RAD_HOME:-} ]]; then
+        echo "RAD_HOME is unset"
+        return 1
+    fi
     rad_profile=$(rad self --profile)
-    cp --force /usr/local/bin/pre-receive /app/radicle/$rad_profile/git/hooks/pre-receive
-    cp --force /usr/local/bin/post-receive /app/radicle/$rad_profile/git/hooks/post-receive
+    cp --force /usr/local/bin/pre-receive "${RAD_HOME}/${rad_profile}/git/hooks/pre-receive"
+    cp --force /usr/local/bin/post-receive "${RAD_HOME}/${rad_profile}/git/hooks/post-receive"
     exec /usr/local/bin/radicle-git-server "$@"
 }
 
