8c6b8e44d2623e57b032a24484fa872131109b2b
xphoniex <dj.2dixx@gmail.com>
2022-08-10 14:10:34 +0000

    http-api: Add tests for `patches` and `issues` in `/v1/projects/`
    Signed-off-by: xphoniex <dj.2dixx@gmail.com>

--- http-api/src/v1/projects.rs
+++ http-api/src/v1/projects.rs
@@ -763,8 +763,8 @@ fn resolve_revisions(
 }
 
 mod setup {
-    use std::env;
     use std::path::Path;
+    use std::{env, fs};
 
     use git2::Oid;
 
@@ -776,6 +776,8 @@ mod setup {
     use librad::git_ext::tree;
     use librad::profile::{Profile, LNK_HOME};
 
+    use radicle_common::cobs::patch::MergeTarget;
+    use radicle_common::cobs::shared::Store;
     use radicle_common::{keys, person, profile, project, test};
 
     #[allow(dead_code)]
@@ -789,13 +791,14 @@ mod setup {
 
         let name = "cloudhead";
         let pass = Pwhash::new(SecUtf8::from(test::USER_PASS), *KDF_PARAMS_TEST);
-        let (profile, _peer_id) = profile::create(profile::home(), pass.clone()).unwrap();
+        let (profile, peer_id) = profile::create(profile::home(), pass.clone()).unwrap();
         let signer = test::signer(&profile, pass).unwrap();
         let storage = keys::storage(&profile, signer.clone()).unwrap();
         let person = person::create(&profile, name, signer.clone(), &storage).unwrap();
 
         person::set_local(&storage, &person).unwrap();
 
+        let whoami = person::local(&storage).unwrap();
         let payload = project::payload(
             "nakamoto".to_owned(),
             "Bitcoin light-client".to_owned(),
@@ -816,6 +819,66 @@ mod setup {
         )
         .unwrap();
 
+        // create remote head
+        // otherwise testing `.../patches/:id` would fail
+        fs::create_dir_all(
+            profile
+                .paths()
+                .git_dir()
+                .join("refs")
+                .join("namespaces")
+                .join(&project.urn().to_string().strip_prefix("rad:git:").unwrap())
+                .join("refs")
+                .join("remotes")
+                .join(peer_id.to_string())
+                .join("heads"),
+        )
+        .unwrap();
+
+        fs::write(
+            profile
+                .paths()
+                .git_dir()
+                .join("refs")
+                .join("namespaces")
+                .join(&project.urn().to_string().strip_prefix("rad:git:").unwrap())
+                .join("refs")
+                .join("remotes")
+                .join(peer_id.to_string())
+                .join("heads")
+                .join("master"),
+            format!("{}", commit),
+        )
+        .unwrap();
+
+        // add a patch
+        let cobs = Store::new(whoami, profile.paths(), &storage);
+        let patches = cobs.patches();
+        let target = MergeTarget::Upstream;
+        let base = Oid::from_str("af08e95ada2bb38aadd8e6cef0963ce37a87add3").unwrap();
+        let rev0_oid = Oid::from_str("518d5069f94c03427f694bb494ac1cd7d1339380").unwrap();
+        let project_urn = &project.urn();
+        let _patch_id = patches
+            .create(
+                project_urn,
+                "My first patch",
+                "Blah blah blah.",
+                target,
+                base,
+                rev0_oid,
+                &[],
+            )
+            .unwrap();
+
+        // add an issue
+        let issues = cobs.issues();
+        let issue_id = issues
+            .create(&project.urn(), "My first issue", "Blah blah blah.", &[])
+            .unwrap();
+        issues
+            .comment(&project.urn(), &issue_id, "Ho ho ho.")
+            .unwrap();
+
         (profile, signer, project, commit)
     }
 }
@@ -835,6 +898,8 @@ mod routes {
     const COMMIT_FILE_NAME: &str = "HI";
     const COMMIT_FILE_CONTENT: &str = "Hi Bob";
     const COMMIT_README_CONTENT: &str = "This is a readme";
+    const PATCH_TITLE: &str = "My first patch";
+    const ISSUE_TITLE: &str = "My first issue";
 
     #[tokio::test]
     async fn test_projects_root_route() {
@@ -1015,5 +1080,83 @@ mod routes {
 
         assert_eq!(body["path"], "README");
         assert_eq!(body["content"], COMMIT_README_CONTENT);
+    }
+
+    #[tokio::test]
+    async fn test_patches_route() {
+        let (profile, signer, project, _head) = setup::profile();
+        let ctx = Context::new(profile.paths().to_owned(), signer, THEME.to_string());
+        let app = router(ctx);
+        let response = app
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri(format!("/projects/{}/patches", project.urn()))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+
+        assert_eq!(body[0]["title"], PATCH_TITLE);
+        assert_eq!(body[1], Value::Null);
+
+        let patch_id = body[0]["id"].as_str().unwrap();
+        let response = app
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri(format!("/projects/{}/patches/{}", project.urn(), patch_id))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_issues_route() {
+        let (profile, signer, project, _head) = setup::profile();
+        let ctx = Context::new(profile.paths().to_owned(), signer, THEME.to_string());
+        let app = router(ctx);
+        let response = app
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri(format!("/projects/{}/issues", project.urn()))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+
+        assert_eq!(body[0]["title"], ISSUE_TITLE);
+        assert_eq!(body[1], Value::Null);
+
+        let issue_id = body[0]["id"].as_str().unwrap();
+        let response = app
+            .clone()
+            .oneshot(
+                Request::builder()
+                    .uri(format!("/projects/{}/issues/{}", project.urn(), issue_id))
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
     }
 }
