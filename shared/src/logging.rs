f770a88e9ba4791c2fd95f4b2a2b8d729ff9df4e
Adam Szkoda <adaszko@gmail.com>
2022-08-30 11:21:58 +0200

    Log at the info level by default
    Signed-off-by: Adam Szkoda <adaszko@gmail.com>

--- shared/src/logging.rs
+++ shared/src/logging.rs
@@ -1,9 +1,12 @@
-pub fn init_logger() {
-    use tracing::dispatcher::{self, Dispatch};
-    use tracing_subscriber::layer::SubscriberExt;
-    use tracing_subscriber::Registry;
+use tracing::dispatcher::{self, Dispatch};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::Registry;
 
-    let subscriber = Registry::default().with(tracing_logfmt::layer());
+pub fn init_logger() {
+    let subscriber = Registry::default()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .with(tracing_logfmt::layer());
 
     dispatcher::set_global_default(Dispatch::new(subscriber))
         .expect("Global logger has already been set!");
